2f082067298455b0272f397294e96a06675c7826
joshipriyanshu5 <joshipriyanshu5@gmail.com>
2026-06-29 21:56:05 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-b from-orange-50 to-yellow-100">
+      <div className="text-center px-6">
+        <h1 className="text-5xl font-bold text-orange-700">
+          Samvidha Papad
+        </h1>
+
+        <p className="mt-4 text-xl">
+          Every Bite Carries the Taste of Tradition.
+        </p>
+      </div>
+    </main>
+  );
+}
